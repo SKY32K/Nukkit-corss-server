43fd129d5a9ab5c22fc20145b192ccee2a7e144d
Maomy <142064814+SKY32K@users.noreply.github.com>
2025-01-22 19:59:59 +0800

    Update ConfigManager.java

--- src/com/fast/server/ConfigManager.java
+++ src/com/fast/server/ConfigManager.java
@@ -13,41 +13,24 @@ public class ConfigManager {
         this.plugin = plugin;
     }
 
-    /**
-     * 加载配置文件
-     */
     public void loadConfig() {
-        // 确保插件文件夹存在
         if (!plugin.getDataFolder().exists()) {
             plugin.getDataFolder().mkdirs();
         }
 
-        // 保存默认配置文件（如果不存在）
         plugin.saveResource("config.yml", false);
 
-        // 加载配置文件
         config = new Config(new File(plugin.getDataFolder(), "config.yml"), Config.YAML);
     }
 
-    /**
-     * 获取配置文件
-     *
-     * @return 配置文件实例
-     */
     public Config getConfig() {
         return config;
     }
 
-    /**
-     * 保存配置文件
-     */
     public void saveConfig() {
         config.save();
     }
 
-    /**
-     * 重新加载配置文件
-     */
     public void reloadConfig() {
         config.reload();
     }
